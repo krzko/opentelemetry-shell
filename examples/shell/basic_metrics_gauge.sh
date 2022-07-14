bd9adb6ed9779aa0c1f5628524a6a04e6b95d630
Kristof Kowalski <k@ko.wal.ski>
2022-07-14 21:25:31 +1000

    Add GHA example

--- examples/shell/basic_metrics_gauge.sh
+++ examples/shell/basic_metrics_gauge.sh
@@ -26,42 +26,51 @@ custom_resource_attributes=(
 service_version="0.0.1-dev"
 
 # Export library path
-export OTEL_SH_LIB_PATH="../library"
+export OTEL_SH_LIB_PATH="../../library"
 
 # Import functions
 . "${OTEL_SH_LIB_PATH}/otel_metrics.sh"
 
-# Sum of type double, metric
-log_info "Pushing metric ko.wal.ski/script/usage_time..."
-otel_metrics_push_sum "ko.wal.ski/script/usage_time" \
-  "Script usuage time in seconds." \
+# Gauge of type int, metric
+log_info "Pushing metric ko.wal.ski/brain/memory/used_bytes..."
+otel_metrics_push_gauge "ko.wal.ski/brain/memory/used_bytes" \
+  "Memory usage in bytes." \
+  "By" \
+  "memory_type" \
+  "evictable" \
+  $RANDOM \
+  int
+
+# Gauge of type double, metric
+log_info "Pushing metric ko.wal.ski/person/uptime..."
+otel_metrics_push_gauge "ko.wal.ski/person/uptime" \
+  "Uptime in seconds." \
   "s" \
-  "time" \
-  "latency" \
-  "${RANDOM}.616987465" \
-  double \
-  "$(date +%s)000000000"
+  "person_name" \
+  "foo" \
+  88927.690019019 \
+  double
 
 # Check script errorlevel (success) using gauge of type int, metric
 log_info "Checking errorlevel..."
 if [ $? -eq 0 ]; then
-  log_info "Pushing metric ko.wal.ski/script/is_failed as 0"
+  log_info "Pushing metric ko.wal.ski/${0##*/}/is_failed as 0"
   log_info "${0##*/} ran successfully"
-  otel_metrics_push_gauge "ko.wal.ski/script/is_failed" \
+  otel_metrics_push_gauge "ko.wal.ski/${0##*/}/is_failed" \
     "If the script was successful." \
     "By" \
-    "script_name" \
-    "${0##*/}" \
+    "hostname" \
+    "${hostname}" \
     0 \
     int
 else
-  log_info "Pushing metric ko.wal.ski/script/is_failed as 1"
+  log_info "Pushing metric ko.wal.ski/${0##*/}/is_failed as 1"
   log_error "${0##*/} failed"
-  otel_metrics_push_gauge "ko.wal.ski/script/is_failed" \
+  otel_metrics_push_gauge "ko.wal.ski/${0##*/}/is_failed" \
     "If the script was successful." \
     "By" \
-    "script_name" \
-    "${0##*/}" \
+    "hostname" \
+    "${hostname}" \
     1 \
     int
 fi
